31d6bdc8e2637f997dab8efa1eebc61b32e06e90
Chris Reynolds <chris@jazzsequence.com>
2024-02-08 17:12:23 -0700

    Refactor install-wp-tests.sh script

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -1,49 +1,24 @@
 #!/usr/bin/env bash
-set -x
+set -e
+
 # shellcheck disable=SC1091
 source "$(dirname "$0")/helpers.sh"
 
-if [ $# -lt 3 ]; then
-	usage $0
-	exit 1
-fi
-
-DB_NAME=$1
-DB_USER=$2
-DB_PASS=$3
-DB_HOST=${4-localhost}
-WP_VERSION=${5-latest}
-SKIP_DB_CREATE=${6-false}
-
-TMPDIR=${TMPDIR-/tmp}
+WP_VERSION=${1:-latest}
+TMPDIR=${2:-/tmp}
+DB_NAME=${3:-"wordpress_test"}
+DB_USER=${4:-"root"}
+DB_PASS=${5:-""}
+DB_HOST=${6:-"127.0.0.1"}
+SKIP_DB=${7:-""}
 WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
-if [[ $WP_VERSION =~ ^[0-9]+\.[0-9]+$ ]]; then
-	WP_TESTS_TAG="branches/$WP_VERSION"
-elif [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
-	if [[ $WP_VERSION =~ [0-9]+\.[0-9]+\.[0] ]]; then
-		# version x.x.0 means the first release of the major version, so strip off the .0 and download version x.x
-		WP_TESTS_TAG="tags/${WP_VERSION%??}"
-	else
-		WP_TESTS_TAG="tags/$WP_VERSION"
-	fi
-elif [[ $WP_VERSION == 'nightly' || $WP_VERSION == 'trunk' ]]; then
-	WP_TESTS_TAG="trunk"
-else
-	# http serves a single offer, whereas https serves multiple. we only want one
-	download http://api.wordpress.org/core/version-check/1.7/ /tmp/wp-latest.json
-	grep -Eo '"version":"[0-9]+\.[0-9]+(\.[0-9]+)?' /tmp/wp-latest.json | sed 's/"version":"//'
-	LATEST_VERSION=$(grep -o '"version":"[^"]*' /tmp/wp-latest.json | sed 's/"version":"//')
-	if [[ -z "$LATEST_VERSION" ]]; then
-		echo "Latest WordPress version could not be found"
-		exit 1
-	fi
-	WP_TESTS_TAG="tags/$LATEST_VERSION"
-fi
-
-set -e
-
-install_wp
+download_wp --version="$WP_VERSION" --tmpdir="$TMPDIR"
+setup_wp --version="$WP_VERSION"
 install_test_suite
-install_db
+
+# Maybe install the database.
+if [ -n "$SKIP_DB" ]; then
+	install_db "$DB_NAME" "$DB_USER" "$DB_PASS" "$DB_HOST"
+fi
