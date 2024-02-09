5b2a480c4edfcdfc688c94206e3a232653cec11c
Chris Reynolds <chris@jazzsequence.com>
2024-02-08 17:25:37 -0700

    use flags instead of args

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -4,13 +4,54 @@ set -e
 # shellcheck disable=SC1091
 source "$(dirname "$0")/helpers.sh"
 
-WP_VERSION=${1:-latest}
-TMPDIR=${2:-/tmp}
-DB_NAME=${3:-"wordpress_test"}
-DB_USER=${4:-"root"}
-DB_PASS=${5:-""}
-DB_HOST=${6:-"127.0.0.1"}
-SKIP_DB=${7:-""}
+# Initialize variables with default values
+TMPDIR="/tmp"
+DB_NAME="wordpress_test"
+DB_USER="root"
+DB_PASS=""
+DB_HOST="127.0.0.1"
+WP_VERSION=${WP_VERSION:-latest}
+SKIP_DB=""
+
+# Parse command-line arguments
+for i in "$@"; do
+	case $i in
+		--dbname=*)
+		DB_NAME="${i#*=}"
+		shift
+		;;
+		--dbuser=*)
+		DB_USER="${i#*=}"
+		shift
+		;;
+		--dbpass=*)
+		DB_PASS="${i#*=}"
+		shift
+		;;
+		--dbhost=*)
+		DB_HOST="${i#*=}"
+		shift
+		;;
+		--version=*)
+		WP_VERSION="${i#*=}"
+		shift
+		;;
+		--no-db)
+		SKIP_DB="true"
+		shift
+		;;
+		--tmpdir=*)
+		TMPDIR="${i#*=}"
+		shift
+		;;
+		*)
+		# unknown option
+		echo "Unknown option: $i. Usage: ./bin/install-wp-tests.sh --dbname=wordpress_test --dbuser=root --dbpass=root --dbhost=localhost --version=latest --tmpdir=/tmp --no-db"
+		exit 1
+		;;
+	esac
+done
+
 WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
 WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
 
