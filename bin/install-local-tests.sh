8ed9ff3b220626d5fc0d6ade0075c0ae817c8b6f
Chris Reynolds <chris@jazzsequence.com>
2024-02-13 13:58:29 -0700

    wrap install-local-tests and install-wp-tests in a function

--- bin/install-local-tests.sh
+++ bin/install-local-tests.sh
@@ -4,60 +4,64 @@ set -e
 # shellcheck disable=SC1091
 source "$(dirname "$0")/helpers.sh"
 
-# Initialize variables with default values
-TMPDIR="/tmp"
-DB_NAME="wordpress_test"
-DB_USER="root"
-DB_PASS=""
-DB_HOST="127.0.0.1"
-WP_VERSION=${WP_VERSION:-latest}
-SKIP_DB=""
+main() {
+  # Initialize variables with default values
+  local TMPDIR="/tmp"
+  local DB_NAME="wordpress_test"
+  local DB_USER="root"
+  local DB_PASS=""
+  local DB_HOST="127.0.0.1"
+  local WP_VERSION=${WP_VERSION:-latest}
+  local SKIP_DB=""
 
-# Parse command-line arguments
-for i in "$@"; do
-  case $i in
-    --dbname=*)
-    DB_NAME="${i#*=}"
-    ;;
-    --dbuser=*)
-    DB_USER="${i#*=}"
-    ;;
-    --dbpass=*)
-    DB_PASS="${i#*=}"
-    ;;
-    --dbhost=*)
-    DB_HOST="${i#*=}"
-    ;;
-    --version=*)
-    WP_VERSION="${i#*=}"
-    ;;
-    --skip-db=*)
-    SKIP_DB="true"
-    ;;
-    --tmpdir=*)
-    TMPDIR="${i#*=}"
-    ;;
-    *)
-    # unknown option
-    echo "Unknown option: $i. Usage: ./bin/install-local-tests.sh --dbname=wordpress_test --dbuser=root --dbpass=root --dbhost=localhost --version=latest --tmpdir=/tmp --skip-db=true"
-    exit 1
-    ;;
-  esac
-done
+  # Parse command-line arguments
+  for i in "$@"; do
+    case $i in
+      --dbname=*)
+      DB_NAME="${i#*=}"
+      ;;
+      --dbuser=*)
+      DB_USER="${i#*=}"
+      ;;
+      --dbpass=*)
+      DB_PASS="${i#*=}"
+      ;;
+      --dbhost=*)
+      DB_HOST="${i#*=}"
+      ;;
+      --version=*)
+      WP_VERSION="${i#*=}"
+      ;;
+      --skip-db=*)
+      SKIP_DB="true"
+      ;;
+      --tmpdir=*)
+      TMPDIR="${i#*=}"
+      ;;
+      *)
+      # unknown option
+      echo "Unknown option: $i. Usage: ./bin/install-local-tests.sh --dbname=wordpress_test --dbuser=root --dbpass=root --dbhost=localhost --version=latest --tmpdir=/tmp --skip-db=true"
+      exit 1
+      ;;
+    esac
+  done
 
-# Run install-wp-tests.sh
-echo "Installing local tests into ${TMPDIR}"
-echo "Using WordPress version: ${WP_VERSION}"
+  # Run install-wp-tests.sh
+  echo "Installing local tests into ${TMPDIR}"
+  echo "Using WordPress version: ${WP_VERSION}"
 
-ARGS=(--version="$WP_VERSION" --tmpdir="$TMPDIR" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST")
+  ARGS=(--version="$WP_VERSION" --tmpdir="$TMPDIR" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST")
 
-if [ -n "$SKIP_DB" ]; then
-  echo "Skipping database creation"
-  ARGS=("${ARGS[@]}" --skip-db=true)
-fi
+  if [ -n "$SKIP_DB" ]; then
+    echo "Skipping database creation"
+    ARGS=("${ARGS[@]}" --skip-db=true)
+  fi
 
-bash "$(dirname "$0")/install-wp-tests.sh" "${ARGS[@]}"
+  bash "$(dirname "$0")/install-wp-tests.sh" "${ARGS[@]}"
 
-# Run PHPUnit
-echo "Running PHPUnit"
-composer phpunit
+  # Run PHPUnit
+  echo "Running PHPUnit"
+  composer phpunit
+}
+
+main "$@"
